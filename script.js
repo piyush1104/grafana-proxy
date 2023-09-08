163bed7a034371d3cc52005ae661276ae074073f
Piyush Bansal <19729997+piyush1104@users.noreply.github.com>
2023-09-08 10:28:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ const queryParams = new URLSearchParams(window.location.search);
 const from = queryParams.get('from');
 const to = queryParams.get('to');
 const cluster = queryParams.get('var-cluster');
+const dashboardType = queryParams.get('dashboard_type');
+const app = queryParams.get('app');
+
+// Set the default app value based on the dashboard_type
+if (!app) {
+    if (dashboardType === 'rooms-api') {
+        app = 'room';
+    }
+}
 
 // Construct the Grafana URL with the extracted values
-const grafanaURL = `https://grafana.100ms.live/explore?left={"queries":[{"refId":"A","datasource":{"type":"loki"},"editorMode":"builder","expr":"{cluster_alias=\\"${cluster}\\", app=\\"room\\"} |= \\"\\"","queryType":"range"}],"range":{"from":"${from}","to":"${to}"}}`;
+const grafanaURL = `https://grafana.100ms.live/explore?left={"queries":[{"refId":"A","datasource":{"type":"loki"},"editorMode":"builder","expr":"{cluster_alias=\\"${cluster}\\", app=\\"${app}\\"} |= \\"\\"","queryType":"range"}],"range":{"from":"${from}","to":"${to}"}}`;
 
 // Redirect to the Grafana URL
 window.location.href = grafanaURL;
