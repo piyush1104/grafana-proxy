11b9b4cf1c6eec9ba4ce9892e1a10496eebb544a
Piyush Bansal <19729997+piyush1104@users.noreply.github.com>
2024-12-05 10:55:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,13 @@ if (!app) {
     }
 }
 
+// https://grafana.100ms.live/explore?panes=%7B%226uY%22:%7B%22datasource%22:%22lO4rcexIk%22,%22queries%22:%5B%7B%22refId%22:%22A%22,%22expr%22:%22%22,%22queryType%22:%22range%22,%22datasource%22:%7B%22type%22:%22loki%22,%22uid%22:%22lO4rcexIk%22%7D%7D%5D,%22range%22:%7B%22from%22:%221733309419679%22,%22to%22:%221733358697743%22%7D%7D%7D&schemaVersion=1&orgId=1
+// https://grafana.100ms.live/explore?panes={"6uY":{"datasource":"lO4rcexIk","queries":[{"refId":"A","expr":"","queryType":"range","datasource":{"type":"loki","uid":"lO4rcexIk"}}],"range":{"from":"1733309419679","to":"1733358697743"}}}&schemaVersion=1&orgId=1
+// https://grafana.100ms.live/explore?panes={"6uY":{"datasource":"lO4rcexIk","queries":[{"refId":"A","expr":"{cluster_alias=\"prod-in3\", app=\"roomcode\"} |= ``","queryType":"range","datasource":{"type":"loki","uid":"lO4rcexIk"},"editorMode":"code"}],"range":{"from":"1733309419679","to":"1733358697743"}}}&schemaVersion=1&orgId=1
+
 // Construct the Grafana URL with the extracted values
-const grafanaURL = `https://grafana.100ms.live/explore?left={"queries":[{"refId":"A","datasource":{"type":"loki"},"editorMode":"builder","expr":"{cluster_alias=\\"${cluster}\\", app=\\"${app}\\"} |= \\"\\"","queryType":"range"}],"range":{"from":"${from}","to":"${to}"}}`;
+// const grafanaURL = `https://grafana.100ms.live/explore?left={"queries":[{"refId":"A","datasource":{"type":"loki"},"editorMode":"builder","expr":"{cluster_alias=\\"${cluster}\\", app=\\"${app}\\"} |= \\"\\"","queryType":"range"}],"range":{"from":"${from}","to":"${to}"}}`;
+const grafanaURL = `https://grafana.100ms.live/explore?panes={"6uY":{"datasource":"lO4rcexIk","queries":[{"refId":"A","expr":"{cluster_alias=\\"${cluster}\\", app=\\"${app}\\"} |= ``","queryType":"range","datasource":{"type":"loki","uid":"lO4rcexIk"},"editorMode":"code"}],"range":{"from":"${from}","to":"${to}"}}`;
 
 // Redirect to the Grafana URL
 window.location.href = grafanaURL;
